a4d12b7dc633a710c85fe6e7161a061c5912c5a1
zn <zhounan@abupdate.com>
2018-12-14 17:46:58 +0800

    update-pom

--- src/test/java/com/example/demo/FdfsTest.java
+++ src/test/java/com/example/demo/FdfsTest.java
@@ -28,28 +28,28 @@ public class FdfsTest {
 
     @Test
     public void testUpload() throws FileNotFoundException {
-        File file = new File("D:\\test\\1.jpg");
-        // 上传并且生成缩略图
-        StorePath storePath = this.storageClient.uploadFile(
-                new FileInputStream(file), file.length(), "", null);
-        // 带分组的路径
-        System.out.println(storePath.getFullPath());
-        // 不带分组的路径
-        System.out.println(storePath.getPath());
+//        File file = new File("D:\\test\\1.jpg");
+//        // 上传并且生成缩略图
+//        StorePath storePath = this.storageClient.uploadFile(
+//                new FileInputStream(file), file.length(), "jpg", null);
+//        // 带分组的路径
+//        System.out.println(storePath.getFullPath());
+//        // 不带分组的路径
+//        System.out.println(storePath.getPath());
     }
 
     @Test
     public void testUploadAndCreateThumb() throws FileNotFoundException {
-        File file = new File("D:\\test\\1.jpg");
-        // 上传并且生成缩略图
-        StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
-                new FileInputStream(file), file.length(), "jpg", null);
-        // 带分组的路径
-        System.out.println(storePath.getFullPath());
-        // 不带分组的路径
-        System.out.println(storePath.getPath());
-        // 获取缩略图路径
-        String path = thumbImageConfig.getThumbImagePath(storePath.getPath());
-        System.out.println(path);
+//        File file = new File("D:\\test\\1.jpg");
+//        // 上传并且生成缩略图
+//        StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
+//                new FileInputStream(file), file.length(), "jpg", null);
+//        // 带分组的路径
+//        System.out.println(storePath.getFullPath());
+//        // 不带分组的路径
+//        System.out.println(storePath.getPath());
+//        // 获取缩略图路径
+//        String path = thumbImageConfig.getThumbImagePath(storePath.getPath());
+//        System.out.println(path);
     }
 }
