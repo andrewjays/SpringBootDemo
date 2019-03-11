b9bf45a9a5ff5dbdbb1dda2b8ac8e5dd332b721b
Jay <444685553@qq.com>
2019-03-11 17:46:23 +0800

    add-zipkin

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import zipkin.server.EnableZipkinServer;
 
-@EnableEurekaClient
+//@EnableZipkinServer
+@EnableEurekaServer
 @ServletComponentScan
 @SpringBootApplication
 @EnableSwagger2
