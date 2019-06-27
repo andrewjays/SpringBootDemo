e34ed7c3468850ba00e4b9f1bce3883d48a3b043
Jay <444685553@qq.com>
2019-06-27 17:07:18 +0800

    add-spring-context

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
 
+import com.example.demo.springcontext.RContextTest;
+import com.example.demo.springcontext.ContextTest1;
 import org.springframework.boot.SpringApplication;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.remoting.caucho.HessianProxyFactoryBean;
+import org.springframework.context.ConfigurableApplicationContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -20,7 +21,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
+        // 默认注入名称为类名小写
+        RContextTest RContextTest = run.getBean("RContextTest", RContextTest.class);
+
+        ContextTest1 contextTest1 = run.getBean("contextTest1Impl", ContextTest1.class);
+        String[] s = run.getBeanDefinitionNames();
+        contextTest1.test();
+
+        System.out.println(s.toString());
     }
 
 
---
+++ src/main/java/com/example/demo/springcontext/ContextTest1.java
@@ -0,0 +1,13 @@
+package com.example.demo.springcontext;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jay
+ * @description
+ * @date Created in 2019/6/27 16:46
+ */
+public interface ContextTest1 {
+    void test();
+
+}
