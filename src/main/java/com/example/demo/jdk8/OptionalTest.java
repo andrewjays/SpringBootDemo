f2324449cd02a780200181ae27d1936dc23b1908
Jay <444685553@qq.com>
2019-07-04 10:36:41 +0800

    add-ForkJoin

---
+++ src/main/java/com/example/demo/jdk8/OptionalTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.jdk8;
+
+/**
+ * @author Jay
+ * @description Optional 类(java.util.Optional) 是一个容器类，代表一个值存在或不存在，
+ * 原来用null 表示一个值不存在，现在Optional 可以更好的表达这个概念。并且可以避免空指针异常。
+ * @date Created in 2019/7/4 10:35
+ */
+public class OptionalTest {
+
+}
