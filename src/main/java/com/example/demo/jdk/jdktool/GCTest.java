a05dd2b7153216fef870188ad4ecebee31f0e440
Andrew-zn0 <444685553@qq.com>
2019-07-14 22:09:23 +0800

    update

--- src/main/java/com/example/demo/jdk/jdktool/GCTest.java
+++ src/main/java/com/example/demo/jdk/jdktool/GCTest.java
@@ -17,7 +17,7 @@ package com.example.demo.jdk.jdktool;
 public class GCTest {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        
+
         for (int i = 0; i < 100000000; i++) {
             new Test();
 
