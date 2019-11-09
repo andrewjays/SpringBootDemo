ceb1d48d19b2315cf91eb4beef42b56a00eec588
Jay <444685553@qq.com>
2019-11-09 12:03:57 +0800

    add-queue-test

---
+++ src/main/java/com/example/demo/jdk/test/QueueTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.jdk.test;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @author Jay
+ * @description 阻塞队列
+ * @date Created in 2019/11/7 16:15
+ */
+public class QueueTest {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
